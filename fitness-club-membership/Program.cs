150e5c9dc193e5774131e151f2eebb2a1b013f24
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-27 12:20:29 +1100

    First commit

---
+++ fitness-club-membership/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace fitness_club_membership
+{
+    class Member
+    {
+        protected int annualFee;
+        private string name;
+        private int memberID;
+        private int memberSince;
+
+        public override string ToString()
+        {
+            return "\nName: " + name + "\nMember ID: " + memberID
+                + "\nMember Since: " + memberSince + "\nTotal Annual Fee: "
+                + annualFee;
+        }
+
+        public Member()
+        {
+            Console.WriteLine("Parent Constructor with no parameter");
+        }
+
+        public Member(string pName, int pMemberID, int pMemberSince)
+        {
+            Console.WriteLine("Parent Constructor with 3 parameters");
+
+            name = pName;
+            memberID = pMemberID;
+            memberSince = pMemberSince;
+        }
+    }
+}
